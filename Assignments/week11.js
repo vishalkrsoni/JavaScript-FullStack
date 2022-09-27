0b784b031cdbfd994a79a36059bec059af5bdc0e
vishalkrsoni <vishal.sony1@gmail.com>
2022-09-27 14:00:54 +0530

    w11

---
+++ Assignments/week11.js
@@ -0,0 +1,7 @@
+// Q-1 ) https://leetcode.com/problems/squares-of-a-sorted-array/
+
+const sortedSquares = (nums) => nums.map((item)=>item*item).sort((a,b)=>a-b)
+
+// Q-2 ) https://leetcode.com/problems/reverse-string/
+
+var reverseString = s=> s=s.reverse()
